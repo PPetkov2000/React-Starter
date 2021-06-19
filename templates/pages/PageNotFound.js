5b80f23b95d6347f6db8a4612adb5b5bc7052664
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-06-20 01:01:18 +0300

    update headings

--- templates/pages/PageNotFound.js
+++ templates/pages/PageNotFound.js
@@ -3,8 +3,8 @@ module.exports = `import React from "react"
 function PageNotFound() {
   return (
     <div className="page-not-found">
-      <h1 className="page-not-found-text-code">404</h1>
-      <h2 className="page-not-found-text">Page Not Found</h2>
+      <h2 className="page-not-found-text-code">404</h2>
+      <h3 className="page-not-found-text">Page Not Found</h3>
     </div>
   )
 }
