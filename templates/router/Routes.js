7e7a42f8f3b46f0f0a3da2c8bfc585ecf092ad3d
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-06-29 03:24:27 +0300

    remove styles import

--- templates/router/Routes.js
+++ templates/router/Routes.js
@@ -1,6 +1,5 @@
 module.exports = `import React from "react"
 import { BrowserRouter, Switch, Route } from "react-router-dom"
-import "../App.scss"
 import Navigation from "../components/Navigation"
 import Footer from "../components/Footer"
 import Home from "../pages/Home"
