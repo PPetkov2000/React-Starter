51baa3300f12a91d89b688f29cb1bdbc5c71d822
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-06-29 03:21:36 +0300

    Update Routes.js

--- templates/router/Routes.js
+++ templates/router/Routes.js
@@ -1,10 +1,29 @@
 module.exports = `import React from "react"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+import "../App.scss"
+import Navigation from "../components/Navigation"
+import Footer from "../components/Footer"
+import Home from "../pages/Home"
+import Register from "../pages/Register"
+import Login from "../pages/Login"
+import Forbidden from "../pages/Forbidden"
+import PageNotFound from "../pages/PageNotFound"
 
 const Routes = () => {
   return (
-    <div>
-      <h2>Routes</h2>
-    </div>
+    <BrowserRouter>
+      <Navigation />
+      <main className="page-wrapper">
+        <Switch>
+          <Route path={["/", "/home"]} exact component={Home} />
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
+          <Route path="/forbidden" component={Forbidden} />
+          <Route path="*" component={PageNotFound} />
+        </Switch>
+      </main>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
