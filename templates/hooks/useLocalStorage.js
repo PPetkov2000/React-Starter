f658b0059b9a4fc36c4749a9c25dd3088ac16df1
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-07-26 20:51:14 +0300

    Update useLocalStorage.js

--- templates/hooks/useLocalStorage.js
+++ templates/hooks/useLocalStorage.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+module.exports = `import { useEffect, useState } from "react"
 
 const PREFIX = "instagram-clone-"
 
@@ -24,3 +24,4 @@ const useLocalStorage = (key, initialValue) => {
 }
 
 export default useLocalStorage
+`
